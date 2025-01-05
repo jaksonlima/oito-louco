79ad3abaa2741fea7f33c767858080475827e1a9
jaksonlima <jaksonsneider@gmail.com>
2025-01-05 14:48:40 -0300

    feat: added component winn

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -29,7 +29,7 @@ function NavBar({ children }: PropsWithChildren) {
         <Navbar shouldHideOnScroll isBordered={isDark} variant="sticky">
           <Navbar.Brand>
             <Image
-              src="/poker-3-512x512.png"
+              src="/poker-3.jpg"
               alt="card"
               width={50}
               height={50}
